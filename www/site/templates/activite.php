0f08c168d9dc11df53a5109637c475499942c9f6
Noémie <noemie@velorizons.com>
2023-12-05 22:21:12 +0100

    wip

---
+++ www/site/templates/activite.php
@@ -0,0 +1,12 @@
+<?php snippet('header') ?>
+
+<main class="main">
+    <section>
+        gresqgrfs
+        <h1>heuu<?= $page->title() ?></h1>
+        <p><?= $page->text() ?></p>
+    </section>
+
+</main>
+
+<?php snippet('footer') ?>
