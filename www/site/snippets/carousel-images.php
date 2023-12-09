a137537a36f13de69a80fb6b7223b74d2e8f0872
Noémie <noemie@velorizons.com>
2023-12-09 16:22:49 +0100

    wip

---
+++ www/site/snippets/carousel-images.php
@@ -0,0 +1,14 @@
+<div class="carousel-images">
+COUCOU
+
+<?= $slots->images() ?>
+
+</div>
+<!-- 
+<figure class="card">
+	<div class="figure" style="--ratio: 16/9; --span: 2">
+	</div>
+	<figcaption>
+		<?= $slots->content() ?>
+	</figcaption>
+</figure> -->
