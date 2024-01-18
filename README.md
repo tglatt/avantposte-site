07e52088d21f86b8e7061d0fcd8608c96f3b7372
Noémie <noemie@velorizons.com>
2024-01-18 16:18:13 +0100

    modif: home + contact

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # avantposte-site
+
+
+# Lien pour modifier les cartes
+
+## carte de la section contact
+Here is your secret edit link: https://umap.openstreetmap.fr/fr/map/anonymous-edit/1011588:r6YHWx2n-umZlHLHn89HaS20VQjmuBF-Wu1K6GpCg_8
--- www/site/templates/contact.php
+++ www/site/templates/contact.php
@@ -1,9 +1,39 @@
-
 <?php snippet('header') ?>
 
 <main class="main">
-    <h1><?= $page->title() ?></h1>
-    <p><?= $page->text()->kt() ?></p>
-</main>
+    <!-- Section 1 -->
+    <section>
+        <div class="title">
+            <h1><?= $page->title() ?></h1>
+        </div>
+        <hr>
+        <hr>
+        <p><?= $page->sec1text()->kt() ?></p>
+    </section>
 
-<?php snippet('footer') ?> 
+    <!-- Section 2 -->
+    <section>
+        <div class="info">
+            <h2><?= $page->sec2title2() ?></h2>
+            <hr>
+
+            <div class="two_col">
+                <div class="col1">
+                    <div class="small_p"><?= $page->sec2textCol1()->kt() ?></div>
+                </div>
+                <div class="col2">
+                    <div class="imageInCol h100">
+                        <iframe width="100%" height="100%" frameborder="0" allowfullscreen allow="geolocation" src="//umap.openstreetmap.fr/fr/map/localisation-de-lavant-poste_1011588?scaleControl=false&miniMap=false&scrollWheelZoom=false&zoomControl=true&editMode=disabled&moreControl=true&searchControl=null&tilelayersControl=null&embedControl=null&datalayersControl=true&onLoadPanel=undefined&captionBar=false&captionMenus=true">
+                        </iframe>
+                        <!-- <p>
+                            <a href="//umap.openstreetmap.fr/fr/map/localisation-de-lavant-poste_1011588?scaleControl=false&miniMap=false&scrollWheelZoom=true&zoomControl=true&editMode=disabled&moreControl=true&searchControl=null&tilelayersControl=null&embedControl=null&datalayersControl=true&onLoadPanel=undefined&captionBar=false&captionMenus=true">
+                                Voir en plein écran
+                            </a>
+                        </p> -->
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <?php snippet('footer') ?>
+</main>
